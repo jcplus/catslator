ced9372bb84b48d9ea1308f8173170d489c8e597
unicornboat <unicornboat1994@gmail.com>
2023-05-01 23:04:21 +1000

    Add pdfjs-dist

---
+++ src/utils/pdf.js
@@ -0,0 +1,340 @@
+import * as pdfjsLib from "pdfjs-dist";
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+	"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.5.141/pdf.worker.min.js";
+
+import { computePosition, autoUpdate, shift } from "@floating-ui/dom";
+import { PDFDocumentProxy } from "pdfjs-dist/types/src/display/api";
+// import { AppState, defaultState } from "./components/prompt";
+import { RegPromptValue } from "@src/pages/options/main/RegPrompts";
+import { GPTEventListener, GPTGroup, GPTPageHandler } from "../inject/page";
+import { storage } from "@src/common";
+import { track } from "@src/common/track";
+import { makeButton } from "@src/common/element";
+
+export class PDFProcess implements GPTEventListener {
+	onProgress(gptGroup: GPTGroup): void {}
+	plugin_name = "pdf_process";
+	gptpage?: GPTPageHandler;
+	pdf?: PDFDocumentProxy;
+	text?: string[];
+	stoped = false;
+	process = 0;
+
+	floatcleanup?;
+	callbackGroup: HTMLElement;
+	constructor() {
+		const ctnBt = makeButton("Send 'continue'");
+		const sendPDFBt = makeButton("Send next page");
+		const stopBt = makeButton("Stop");
+		const callback = document.createElement("div");
+		callback.style.display = "none";
+		callback.append(stopBt, ctnBt, sendPDFBt, document.createElement("div"));
+		document.body.appendChild(callback);
+		const that = this;
+		stopBt.addEventListener("click", (e) => {
+			that.stoped = true;
+			callback.style.display = "none";
+			if (that.floatcleanup) {
+				that.floatcleanup();
+				that.floatcleanup = undefined;
+			}
+		});
+		const continueStr = `continue`;
+		ctnBt.addEventListener("click", (e) => {
+			that.gptpage.send(continueStr);
+			callback.style.display = "none";
+			if (that.floatcleanup) {
+				that.floatcleanup();
+				that.floatcleanup = undefined;
+			}
+		});
+		sendPDFBt.addEventListener("click", (e) => {
+			that.process++;
+			that.sendNextPage();
+			callback.style.display = "none";
+			if (that.floatcleanup) {
+				that.floatcleanup();
+				that.floatcleanup = undefined;
+			}
+		});
+		this.callbackGroup = callback;
+		this.callbackGroup.style.position = "absolute";
+	}
+
+	onTextareaCreate(el: HTMLTextAreaElement) {
+		const that = this;
+		const dropzone = el.parentElement as HTMLElement;
+		dropzone.style.border = "2px dashed #ccc";
+		console.log("Try Adding dropable");
+		if (!dropzone.classList.contains("dropable")) {
+			dropzone.addEventListener("dragover", function (e) {
+				(e.target as HTMLElement).style.backgroundColor = "";
+				e.preventDefault();
+				e.stopPropagation();
+				this.style.border = "2px dashed #ccc";
+			});
+			dropzone.addEventListener("dragleave", function (e) {
+				this.style.border = "2px dashed #ccc";
+				e.preventDefault();
+				e.stopPropagation();
+				this.style.border = "2px solid #ccc";
+			});
+			dropzone.addEventListener("drop", function (e: DragEvent) {
+				e.preventDefault();
+				e.stopPropagation();
+				this.style.border = "2px dashed #ccc";
+				if (e.dataTransfer) {
+					const file = e.dataTransfer.files[0];
+					const fn = file.name.toLowerCase();
+					if (fn.endsWith("pdf")) {
+						that.onDropFile(file);
+						track("Drag PDF file", {});
+					} else if (fn.endsWith("pptx")) {
+					} else {
+						that.onDropText(file);
+						const suff = fn.split(".");
+						track("Drag other file", { suff: suff[suff.length - 1] });
+					}
+				} else {
+				}
+			});
+			dropzone.classList.add("dropable");
+			console.log("Add dropable");
+			console.log(dropzone);
+		}
+	}
+
+	bindGPT(gptpage: GPTPageHandler): void {
+		this.gptpage = gptpage;
+		this.gptpage.vote(this);
+		console.log("bind pdf processor");
+	}
+	onSwitchPage(old: boolean): void {}
+
+	onHooked() {}
+	onUnHooked() {}
+
+	onResponse(gptGroup: GPTGroup) {
+		const that = this;
+
+		setTimeout(() => {
+			if (that.pdf) {
+				this.callbackGroup.style.display = "block";
+				this.callbackGroup.lastChild.textContent = `${this.process + 1}/${
+					this.pdf.numPages
+				}`;
+				that.floatcleanup = autoUpdate(
+					gptGroup.markdownRoot,
+					this.callbackGroup,
+					() => {
+						computePosition(gptGroup.response, this.callbackGroup, {
+							placement: "bottom",
+							middleware: [shift({ crossAxis: true })],
+						}).then(({ x, y }) => {
+							Object.assign(this.callbackGroup.style, {
+								left: `${x}px`,
+								top: `${y}px`,
+							});
+						});
+					}
+				);
+				// that.sendNextPage();
+			} else if (that.text) {
+				that.process++;
+				that.sendNextText();
+			}
+		}, 1000);
+	}
+	onSendStart() {
+		// throw new Error("Method not implemented.");
+	}
+	onStopGeneration() {
+		this.callbackGroup.style.display = "block";
+		this.callbackGroup.lastChild.textContent = `${this.process + 1}/${
+			this.pdf.numPages
+		}`;
+		this.floatcleanup = autoUpdate(
+			this.gptpage.lastGroup.markdownRoot,
+			this.callbackGroup,
+			() => {
+				computePosition(this.gptpage.lastGroup.response, this.callbackGroup, {
+					placement: "bottom",
+					middleware: [shift({ crossAxis: true })],
+				}).then(({ x, y }) => {
+					Object.assign(this.callbackGroup.style, {
+						left: `${x}px`,
+						top: `${y}px`,
+					});
+				});
+			}
+		);
+	}
+	onDropText(file: File) {
+		console.log("onDropFile");
+		console.log(file);
+		const that = this;
+		var reader = new FileReader();
+
+		reader.onload = function (event) {
+			var textContent = event.target!.result as string;
+			that.text = textContent.split("\n\n");
+			// 这里可以对读取到的文本内容进行处理
+			that.pdf = undefined;
+			that.stoped = false;
+			that.process = -1;
+			that.sendNextText();
+		};
+		reader.readAsText(file);
+	}
+	onDropFile(file: File) {
+		const that = this;
+		var reader = new FileReader();
+
+		reader.onload = function (event) {
+			var arrayBuffer = event.target!.result as ArrayBuffer;
+			var data = new Uint8Array(arrayBuffer!);
+			var loadingTask = pdfjsLib.getDocument(data!);
+
+			loadingTask.promise.then(
+				function (pdf) {
+					that.pdf = pdf;
+					that.text = undefined;
+					that.stoped = false;
+					that.process = 0;
+					console.log("load pdf success");
+					that.sendNextPage();
+				},
+				function (reason) {
+					console.error(reason);
+				}
+			);
+		};
+		reader.readAsArrayBuffer(file);
+	}
+
+	sendNextText() {
+		const gptPage = this.gptpage!;
+		gptPage.vote(this);
+		if (this.stoped) {
+			console.log("if (this.stoped) {");
+			return;
+		}
+
+		if (!this.text) {
+			console.log("if (!this.pdf) {");
+			return;
+		}
+
+		let page_n = this.process;
+		if (page_n >= this.text.length) {
+			console.log("if (page_n >= this.pdf.numPages) {");
+			return;
+		}
+
+		const that = this;
+		storage
+			.get<RegPromptValue>("currentRegPrompt", {
+			title: "default",
+			prefix: "未找到可用 Prompt",
+			contents: [],
+		})
+			.then((regPrompt) => {
+				const page_str = that.text![that.process + 1];
+				const matchedPrompt = regPrompt.contents
+					.map((item) => {
+						const pattern = new RegExp(item.regex);
+						return {
+							pattern: pattern,
+							match: pattern.exec(page_str.toLowerCase()),
+							prompt: item.prompt,
+						};
+					})
+					.filter((item) => {
+						console.log(item);
+						return item.match;
+					});
+
+				let prompt_str = "";
+				if (matchedPrompt.length == 0) {
+					that.process++;
+					that.sendNextText();
+					return;
+				}
+				prompt_str =
+					matchedPrompt
+						.map((item) => {
+							let prompt = item.prompt;
+							for (let i = 1; i < item.match!.length; i++) {
+								prompt = prompt.replace(`$${i}`, item.match![i]);
+							}
+							return ` - ${prompt}`;
+						})
+						.join("\n") + "\n";
+				prompt_str = `\n${regPrompt.prefix}\n${prompt_str}`;
+				gptPage.send(page_str + prompt_str);
+			});
+	}
+
+	sendNextPage() {
+		const gptPage = this.gptpage!;
+		gptPage.vote(this);
+		if (this.stoped) {
+			console.log("if (this.stoped) {");
+			return;
+		}
+
+		if (!this.pdf) {
+			console.log("if (!this.pdf) {");
+			return;
+		}
+
+		const page_n = this.process;
+		if (page_n >= this.pdf.numPages) {
+			console.log("if (page_n >= this.pdf.numPages) {");
+			return;
+		}
+
+		this.pdf.getPage(page_n + 1).then(function (page) {
+			page.getTextContent().then(function (page_content) {
+				storage
+					.get<RegPromptValue>("currentRegPrompt", {
+					title: "default",
+					prefix: "未找到可用 Prompt",
+					contents: [],
+				})
+					.then((regPrompt) => {
+						const page_str = page_content.items
+							.map((i) => (i as any).str)
+					.join(" ");
+						const matchedPrompt = regPrompt.contents
+							.map((item) => {
+								const pattern = new RegExp(item.regex);
+								return {
+									pattern: pattern,
+									match: pattern.exec(page_str.toLowerCase()),
+									prompt: item.prompt,
+								};
+							})
+							.filter((item) => {
+								console.log(item);
+								return item.match;
+							});
+
+						let prompt_str = "";
+						prompt_str =
+							matchedPrompt
+								.map((item) => {
+									let prompt = item.prompt;
+									for (let i = 1; i < item.match!.length; i++) {
+										prompt = prompt.replace(`$${i}`, item.match![i]);
+									}
+									return ` - ${prompt}`;
+								})
+								.join("\n") + "\n";
+						prompt_str = `\n${regPrompt.prefix}\n${prompt_str}`;
+						gptPage.send(prompt_str + page_str);
+					});
+			});
+		});
+	}
+}
